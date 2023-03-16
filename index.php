9395e9430bd893bfb596b62d8a2776de384ce4eb
DaCipher <73401065+DaCipher@users.noreply.github.com>
2023-03-16 08:52:46 +0100

    Side Hustle 7.0 Week 2 assessment

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+$pvc = false; // Accepts True or false, 1 or 0
+$age = 10;
+$ward = 21;
+
+$errors = [
+  'age' => "",
+  'pvc' => "",
+  'ward' => ""
+]; // Error Bag
+
+if (!$pvc) {
+  $errors['pvc'] = "You don't have PVC!\n";
+}
+
+if ($age < 18) {
+  $errors['age'] = "You must be at least 18years to vote!\n";
+}
+
+if ($ward != 20) {
+  $errors['ward'] = "You must be from ward 20 to vote!\n";
+}
+
+$pvcstatus = (!$pvc) ? "Not Enrolled\n" : "Enrolled\n"; // Customise PVC Status
+echo "YOUR DETAILS\n";
+echo "PVC: $pvcstatus";
+echo "Age: $age\n";
+echo "Ward: $ward\n\n";
+echo "YOUR STATUS\n";
+
+// Check if there are errors.
+if (empty($errors['pvc']) && empty($errors['age']) & empty($errors['ward'])) {
+  echo "Hurray! You are eligible to vote!\n";
+} else {
+  // Loop through the error bag
+  foreach ($errors as $error) {
+    //Find error bags that are not empty
+    if (!empty($error)) {
+      echo "Error: $error";
+    }
+  }
+}
